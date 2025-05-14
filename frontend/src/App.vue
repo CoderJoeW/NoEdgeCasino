4b079e46f699f612c6af92d43b9b95d91becd2f6
Joseph <jwilliamson@jslcodelabs.com>
2025-05-14 11:59:51 -0400

    include auth store on all pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { useAuthStore } from './store/authStore';
+
+const auth = useAuthStore();
+auth.fetchUser();
 </script>
 
 <template>
