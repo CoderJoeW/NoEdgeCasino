4201b0f51d6c355d2dca0552bb4c37f25e74739c
Joseph <jwilliamson@jslcodelabs.com>
2025-05-14 01:00:57 -0400

    scafolding to start testing game implementations

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,9 +1,33 @@
 <template>
-    <p>Hello Home!</p>
+    <p class="text-3xl font-bold underline">NoEdgeCash Balance: {{ globalPoolData.NoEdgeCash.value }}</p>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Home',
+    data() {
+        return {
+            globalPoolData: null,
+        };
+    },
+    async mounted() {
+        await this.loadGlobalPoolData();
+    },
+    methods: {
+        async loadGlobalPoolData() {
+            let response = await axios.get('http://localhost:8000/api/globalPool/balance');
+
+            const mapped = {};
+            response.data.forEach(item => {
+                mapped[item.currency_name] = {
+                    value: Number(item.currency_value)
+                };
+            });
+
+            this.globalPoolData = mapped;
+        },
+    }
 }
 </script>
