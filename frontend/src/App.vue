84ea60aaebca6f7648702e92bc64c6a0c9cf569c
Joseph <jwilliamson@jslcodelabs.com>
2025-05-14 23:10:15 -0400

    move all styling and structure to the page and outside of app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,8 +7,5 @@ auth.fetchUser();
 </script>
 
 <template>
-    <RouterLink to="/">Home</RouterLink>
-    <div class="bg-gradient-to-br from-gray-900 to-slate-800">
-        <RouterView />
-    </div>
+    <RouterView />
 </template>
