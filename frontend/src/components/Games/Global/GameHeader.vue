599b0e133dc21e5155475c4df6ea5ac7d82c7567
Joseph <jwilliamson@jslcodelabs.com>
2025-05-16 01:41:36 -0400

    reduce padding slightly

--- frontend/src/components/Games/Global/GameHeader.vue
+++ frontend/src/components/Games/Global/GameHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gradient-to-r from-rose-600 to-pink-600 p-5 flex justify-between items-center">
+    <div class="bg-gradient-to-r from-rose-600 to-pink-600 p-2 flex justify-between items-center">
         <h1 class="text-2xl font-extrabold tracking-wider text-white m-0 drop-shadow-md">{{ title }}</h1>
         <div class="bg-black/30 px-4 py-2 rounded-full font-semibold text-white">
             <span>Global Pool: ${{ globalBalance?.balances?.NoEdgeCash?.value?.toLocaleString() }}</span>
