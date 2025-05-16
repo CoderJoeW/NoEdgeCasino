3371ee7ff9c81bb7c230f3b395cc1ba17de979c4
Joseph <jwilliamson@jslcodelabs.com>
2025-05-16 01:15:39 -0400

    fix money values to store larger numbers with larger decimal places

---
+++ backend/database/migrations/2025_05_16_045647_increase_global_pool_number_support.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('global_pools', function (Blueprint $table){
+            $table->decimal('currency_value', 21, 8)->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
